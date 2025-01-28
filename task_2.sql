cfee40a76b5a113f25f3cf5c505ff65faebcd627
wilson <mutidawilz@gmail.com>
2025-01-28 09:03:49 +0300

    Added the file task_2

--- task_2.sql
+++ task_2.sql
@@ -32,7 +32,7 @@ CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT,
+    quantity INT, -- Added quantity column here
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
