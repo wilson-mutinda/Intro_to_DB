642e888977d0a7837ab5293571b4fa2b617b7d37
wilson <mutidawilz@gmail.com>
2025-01-28 07:32:55 +0300

    modified the store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,17 +1,17 @@
--- Database creation
-CREATE DATABASE alx_book_store;
+-- Check if the database exists, if not, create it
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 -- Use the created database
 USE alx_book_store;
 
 -- Authors Table
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Books Table
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE Books (
 );
 
 -- Customers Table
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE Customers (
 );
 
 -- Orders Table
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE Orders (
 );
 
 -- Order_Details Table
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
@@ -45,4 +45,3 @@ CREATE TABLE Order_Details (
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
