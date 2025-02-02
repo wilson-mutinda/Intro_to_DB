ed579114b1fef4731a4974221bd6560f24e969a7
wilson <mutidawilz@gmail.com>
2025-02-02 17:12:07 +0300

    Modofied my git file

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,9 @@
--- Get the database name as an argument
-SET @database_name = 'alx_book_store'; 
+USE alx_book_store;
 
--- Construct the dynamic SQL query
-SET @sql = CONCAT('SELECT COLUMN_NAME, COLUMN_TYPE 
-                    FROM INFORMATION_SCHEMA.COLUMNS 
-                    WHERE TABLE_SCHEMA = ''', @database_name, ''' 
-                    AND TABLE_NAME = ''Books'';');
-
--- Prepare and execute the dynamic query
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
